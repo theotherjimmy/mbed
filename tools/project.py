c3b8fa373e41c58eebd72a1425863e59955d59df
Jimmy Brisson <theotherjimmy@gmail.com>
2016-07-22 13:49:22 -0500

    fixing more linting, just project.py

--- tools/project.py
+++ tools/project.py
@@ -1,3 +1,6 @@
+""" The CLI entry point for exporting projects from the mbed tools to any of the
+supported IDEs or project structures.
+"""
 import sys
 from os.path import join, abspath, dirname, exists, basename
 ROOT = abspath(join(dirname(__file__), ".."))
@@ -12,15 +15,28 @@ from tools.export import EXPORTERS, mcu_ide_matrix
 from tools.tests import TESTS, TEST_MAP
 from tools.tests import test_known, test_name_known, Test
 from tools.targets import TARGET_NAMES
-from utils import argparse_filestring_type, argparse_many
-from utils import argparse_force_lowercase_type, argparse_force_uppercase_type
-from project_api import export_project, zip_export, prepare_project, subtract_basepath
+from tools.utils import argparse_filestring_type, argparse_many
+from tools.utils import argparse_force_lowercase_type
+from tools.utils import argparse_force_uppercase_type
+from tools.project_api import export_project, zip_export, prepare_project
+from tools.project_api import subtract_basepath
 
 
 def setup_project(ide, target, program=None, source_dir=None, build=None):
-    # Some libraries have extra macros (called by exporter symbols) to we need to pass
-    # them to maintain compilation macros integrity between compiled library and
-    # header files we might use with it
+    """Generate a name, if not provided, and find dependencies
+
+    Positional arguments:
+    ide - IDE or project structure that will soon be exported to
+    target - MCU that the project will build for
+
+    Keyword arguments:
+    program - the index of a test program
+    source_dir - the directory, or directories that contain all of the sources
+    build - a directory that will contain the result of the export
+    """
+    # Some libraries have extra macros (called by exporter symbols) to we need
+    # to pass them to maintain compilation macros integrity between compiled
+    # library and header files we might use with it
     if source_dir:
         # --source is used to generate IDE files to toolchain directly
         # in the source tree and doesn't generate zip file
@@ -30,17 +46,15 @@ def setup_project(ide, target, program=None, source_dir=None, build=None):
         else:
             project_name = basename(normpath(source_dir[0]))
         src_paths = source_dir
-        lib_paths=None
+        lib_paths = None
     else:
         test = Test(program)
         if not build:
-            # Substitute the library builds with the sources
-            # TODO: Substitute also the other library build paths
+            # Substitute the mbed library builds with their sources
             if MBED_LIBRARIES in test.dependencies:
                 test.dependencies.remove(MBED_LIBRARIES)
                 test.dependencies.append(MBED_BASE)
 
-        # Build the project with the same directory structure of the mbed online IDE
         src_paths = [test.source_dir]
         lib_paths = test.dependencies
         project_name = "_".join([test.id, ide, target])
@@ -49,27 +63,34 @@ def setup_project(ide, target, program=None, source_dir=None, build=None):
     return project_dir, project_name, src_paths, lib_paths
 
 
-def perform_export(target, ide, build=None, src=None, macros=None,
-                   project_id=None, clean=False, zip=False):
-    export, name, src, lib = setup_project(ide, target, program=project_id,
+def export(target, ide, build=None, src=None, macros=None, project_id=None,
+            clean=False, zip_proj=False):
+    """Do an export of a project.
+
+    Positional arguments:
+    target - MCU that the project will compile for
+    """
+    project_dir, name, src, lib = setup_project(ide, target, program=project_id,
                                            source_dir=src, build=build)
 
-    resources, toolchain = prepare_project(src, export, target, ide,
-                          clean=clean, name=name, macros=macros,
-                          libraries_paths=lib)
+    resources, toolchain = prepare_project(src, project_dir, target, ide,
+                                           clean=clean, name=name,
+                                           macros=macros, libraries_paths=lib)
 
     temp = copy.deepcopy(resources)
-    if zip:
+    if zip_proj:
         subtract_basepath(resources, export)
     else:
         resources.relative_to(export)
 
-    files = export_project(resources, export, target, name, toolchain, ide, macros=macros)
-    if zip:
+    files = export_project(resources, export, target, name, toolchain, ide,
+                            macros=macros)
+    if zip_proj:
         zip_export(export + ".zip", name, temp, files)
 
 
-if __name__ == '__main__':
+def main():
+    """Entry point"""
     # Parse Options
     parser = ArgumentParser()
 
@@ -79,16 +100,19 @@ if __name__ == '__main__':
     toolchainlist.sort()
 
     parser.add_argument("-m", "--mcu",
-                      metavar="MCU",
-                      default='LPC1768',
-                      type=argparse_many(argparse_force_uppercase_type(targetnames, "MCU")),
-                      help="generate project for the given MCU (%s)"% ', '.join(targetnames))
+                        metavar="MCU",
+                        default='LPC1768',
+                        type=argparse_many(
+                            argparse_force_uppercase_type(targetnames, "MCU")),
+                        help="generate project for the given MCU ({})".format(
+                            ', '.join(targetnames)))
 
     parser.add_argument("-i",
-                      dest="ide",
-                      default='uvision',
-                      type=argparse_force_lowercase_type(toolchainlist, "toolchain"),
-                      help="The target IDE: %s"% str(toolchainlist))
+                        dest="ide",
+                        default='uvision',
+                        type=argparse_force_lowercase_type(
+                            toolchainlist, "toolchain"),
+                        help="The target IDE: %s"% str(toolchainlist))
 
     parser.add_argument("-c", "--clean",
                       action="store_true",
@@ -96,10 +120,11 @@ if __name__ == '__main__':
                       help="clean the export directory")
 
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument("-p",
-                      type=test_known,
-                      dest="program",
-                      help="The index of the desired test program: [0-%d]"% (len(TESTS)-1))
+    group.add_argument(
+        "-p",
+        type=test_known,
+        dest="program",
+        help="The index of the desired test program: [0-{}]"% (len(TESTS)-1))
 
     group.add_argument("-n",
                       type=test_name_known,
@@ -146,7 +171,7 @@ if __name__ == '__main__':
 
     # Print available tests in order and exit
     if options.list_tests is True:
-        print '\n'.join(map(str, sorted(TEST_MAP.values())))
+        print '\n'.join([str(test) for test in  sorted(TEST_MAP.values())])
         sys.exit()
 
     # Only prints matrix of supported IDEs
@@ -158,13 +183,13 @@ if __name__ == '__main__':
     if options.supported_ides_html:
         html = mcu_ide_matrix(verbose_html=True)
         try:
-            with open("./export/README.md","w") as f:
-                f.write("Exporter IDE/Platform Support\n")
-                f.write("-----------------------------------\n")
-                f.write("\n")
-                f.write(html)
-        except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            with open("./export/README.md", "w") as readme:
+                readme.write("Exporter IDE/Platform Support\n")
+                readme.write("-----------------------------------\n")
+                readme.write("\n")
+                readme.write(html)
+        except IOError as exc:
+            print "I/O error({0}): {1}".format(exc.errno, exc.strerror)
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
@@ -175,17 +200,15 @@ if __name__ == '__main__':
         if exists(EXPORT_DIR):
             rmtree(EXPORT_DIR)
 
-    # Export results
-    successes = []
-    failures = []
-
     for mcu in options.mcu:
-        # Program Number or name
-        p, src, ide = options.program, options.source_dir, options.ide
-
-        zip = src is not []  # create zip when no src_dir provided
+        zip_proj = options.source_dir is not []
 
         # Export to selected toolchain
         #lib_symbols = get_lib_symbols(options.macros, src, p)
-        perform_export(mcu, ide, build=options.build, src=src,
-                       macros=options.macros, project_id=p, clean=options.clean, zip=zip)
+        export(mcu, options.ide, build=options.build, src=options.source_dir,
+               macros=options.macros, project_id=options.project,
+               clean=options.clean, zip=zip_proj)
+
+
+if __name__ == "__main__":
+    main()
