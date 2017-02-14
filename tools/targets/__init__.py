abfab9f666e59f14003a932a6bc75d0538801be4
Jimmy Brisson <theotherjimmy@gmail.com>
2017-02-14 14:22:49 -0600

    Enable Legacy Builds

--- tools/targets/__init__.py
+++ tools/targets/__init__.py
@@ -191,9 +191,7 @@ class Target(namedtuple(
 
     # Default location of the 'targets.json' file
     __targets_json_location_default = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        '..', '..', 'targets', 'targets.json'
-    )
+        os.path.dirname(os.path.abspath(__file__)), '..', 'latest_targets.json')
 
     # Current/new location of the 'targets.json' file
     __targets_json_location = None
