e0d2029ed55d8a9b1c3a28a399f4296c892e8537
Jimmy Brisson <theotherjimmy@gmail.com>
2018-05-09 13:45:34 -0500

    Format options.py with Black
    It looks much nicer now

--- tools/options.py
+++ tools/options.py
@@ -23,17 +23,21 @@ from argparse import ArgumentParser, ArgumentTypeError
 
 from .toolchains import TOOLCHAINS
 from .targets import TARGET_NAMES, Target, update_target_data
-from .utils import (argparse_force_uppercase_type, argparse_deprecate,
-                    argparse_lowercase_hyphen_type, argparse_many,
-                    argparse_filestring_type, args_error,
-                    argparse_profile_filestring_type)
+from .utils import (
+    argparse_force_uppercase_type,
+    argparse_deprecate,
+    argparse_lowercase_hyphen_type,
+    argparse_many,
+    argparse_filestring_type,
+    args_error,
+    argparse_profile_filestring_type,
+)
 
-FLAGS_DEPRECATION_MESSAGE = "Please use the --profile argument instead.\n"\
-                            "Documentation may be found in "\
-                            "docs/Toolchain_Profiles.md"
+FLAGS_DEPRECATION_MESSAGE = """Please use the --profile argument instead.
+Documentation may be found in docs/Toolchain_Profiles.md"""
 
-def get_default_options_parser(add_clean=True, add_options=True,
-                               add_app_config=False):
+
+def get_default_options_parser(add_clean=True, add_options=True, add_app_config=False):
     """Create a new options parser with the default compiler options added
 
     Keyword arguments:
@@ -47,58 +51,89 @@ def get_default_options_parser(add_clean=True, add_options=True,
     toolchainlist = list(TOOLCHAINS)
     toolchainlist.sort()
 
-    parser.add_argument("-m", "--mcu",
-                        help=("build for the given MCU (%s)" %
-                              ', '.join(targetnames)),
-                        metavar="MCU")
+    parser.add_argument(
+        "-m",
+        "--mcu",
+        help=("build for the given MCU (%s)" % ", ".join(targetnames)),
+        metavar="MCU",
+    )
 
-    parser.add_argument("-t", "--tool",
-                        help=("build using the given TOOLCHAIN (%s)" %
-                              ', '.join(toolchainlist)),
-                        metavar="TOOLCHAIN",
-                        type=argparse_many(
-                            argparse_force_uppercase_type(
-                                toolchainlist, "toolchain")))
+    parser.add_argument(
+        "-t",
+        "--tool",
+        help=("build using the given TOOLCHAIN (%s)" % ", ".join(toolchainlist)),
+        metavar="TOOLCHAIN",
+        type=argparse_many(argparse_force_uppercase_type(toolchainlist, "toolchain")),
+    )
 
-    parser.add_argument("--color",
-                        help="print Warnings, and Errors in color",
-                        action="store_true", default=False)
+    parser.add_argument(
+        "--color",
+        help="print Warnings, and Errors in color",
+        action="store_true",
+        default=False,
+    )
 
-    parser.add_argument("--cflags",
-                        type=argparse_deprecate(FLAGS_DEPRECATION_MESSAGE),
-                        help="Deprecated. " + FLAGS_DEPRECATION_MESSAGE)
+    parser.add_argument(
+        "--cflags",
+        type=argparse_deprecate(FLAGS_DEPRECATION_MESSAGE),
+        help="Deprecated. " + FLAGS_DEPRECATION_MESSAGE,
+    )
 
-    parser.add_argument("--asmflags",
-                        type=argparse_deprecate(FLAGS_DEPRECATION_MESSAGE),
-                        help="Deprecated. " + FLAGS_DEPRECATION_MESSAGE)
+    parser.add_argument(
+        "--asmflags",
+        type=argparse_deprecate(FLAGS_DEPRECATION_MESSAGE),
+        help="Deprecated. " + FLAGS_DEPRECATION_MESSAGE,
+    )
 
-    parser.add_argument("--ldflags",
-                        type=argparse_deprecate(FLAGS_DEPRECATION_MESSAGE),
-                        help="Deprecated. " + FLAGS_DEPRECATION_MESSAGE)
+    parser.add_argument(
+        "--ldflags",
+        type=argparse_deprecate(FLAGS_DEPRECATION_MESSAGE),
+        help="Deprecated. " + FLAGS_DEPRECATION_MESSAGE,
+    )
 
     if add_clean:
-        parser.add_argument("-c", "--clean", action="store_true", default=False,
-                            help="clean the build directory")
+        parser.add_argument(
+            "-c",
+            "--clean",
+            action="store_true",
+            default=False,
+            help="clean the build directory",
+        )
 
     if add_options:
-        parser.add_argument("--profile", dest="profile", action="append",
-                            type=argparse_profile_filestring_type,
-                            help="Build profile to use. Can be either path to json" \
-                            "file or one of the default one ({})".format(", ".join(list_profiles())),
-                            default=[])
+        parser.add_argument(
+            "--profile",
+            dest="profile",
+            action="append",
+            type=argparse_profile_filestring_type,
+            help="Build profile to use. Can be either path to json"
+            "file or one of the default one ({})".format(", ".join(list_profiles())),
+            default=[],
+        )
     if add_app_config:
-        parser.add_argument("--app-config", default=None, dest="app_config",
-                            type=argparse_filestring_type,
-                            help="Path of an app configuration file (Default is to look for 'mbed_app.json')")
+        parser.add_argument(
+            "--app-config",
+            default=None,
+            dest="app_config",
+            type=argparse_filestring_type,
+            help="Path of an app configuration file (Default is to look for 'mbed_app.json')",
+        )
 
     return parser
+
 
 def list_profiles():
     """Lists available build profiles
 
-    Checks default profile directory (mbed-os/tools/profiles/) for all the json files and return list of names only
+    Checks default profile directory (mbed-os/tools/profiles/) for all the json files
+    and return list of names only
     """
-    return [fn.replace(".json", "") for fn in listdir(join(dirname(__file__), "profiles")) if fn.endswith(".json")]
+    return [
+        fn.replace(".json", "")
+        for fn in listdir(join(dirname(__file__), "profiles"))
+        if fn.endswith(".json")
+    ]
+
 
 def extract_profile(parser, options, toolchain, fallback="develop"):
     """Extract a Toolchain profile from parsed options
@@ -109,18 +144,20 @@ def extract_profile(parser, options, toolchain, fallback="develop"):
     toolchain - the toolchain that the profile should be extracted for
     """
     profiles = []
-    filenames = options.profile or [join(dirname(__file__), "profiles",
-                                         fallback + ".json")]
+    filenames = options.profile or [
+        join(dirname(__file__), "profiles", fallback + ".json")
+    ]
     for filename in filenames:
         contents = load(open(filename))
         if toolchain not in contents:
-            args_error(parser, ("argument --profile: toolchain {} is not"
-                                " supported by profile {}").format(toolchain,
-                                                                   filename))
+            fmt_str = "argument --profile: toolchain {} is not" " supported by profile {}"
+            msg = fmt_str.format(toolchain, filename)
+            args_error(parser, msg)
         profiles.append(contents)
 
     return profiles
-    
+
+
 def extract_mcus(parser, options):
     try:
         if options.source_dir:
@@ -132,7 +169,7 @@ def extract_mcus(parser, options):
     targetnames = TARGET_NAMES
     targetnames.sort()
     try:
-        return argparse_many(argparse_force_uppercase_type(targetnames, "MCU"))(options.mcu)
+        type = argparse_many(argparse_force_uppercase_type(targetnames, "MCU"))
+        return type(options.mcu)
     except ArgumentTypeError as exc:
         args_error(parser, "argument -m/--mcu: {}".format(str(exc)))
-
