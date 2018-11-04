881c4af98f2e72a46b0c9d10457bfd35056cb260
Oren Cohen <oren.cohen@arm.com>
2018-11-04 19:04:43 +0200

    Add Cypress memory protection
    memory protection init has been moved to spm_hal_memory_protection_init from mbed_sdk_init
    
    88204c7861a9c3d4ad829982c067e08dba130a9e

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_FUTURE_SEQUANA_M0_PSA/spm_hal_api.c
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_FUTURE_SEQUANA_M0_PSA/spm_hal_api.c
@@ -17,8 +17,11 @@
 /* -------------------------------------- Includes ----------------------------------- */
 
 #include "cy_sysint.h"
-
 #include "spm_internal.h"
+
+#ifdef PU_ENABLE
+#include "cyprotection_config.h"
+#endif // PU_ENABLE
 
 
 /* -------------------------------------- HAL API ------------------------------------ */
@@ -32,4 +35,42 @@ void spm_hal_start_nspe(void)
 
 void spm_hal_memory_protection_init(void)
 {
+#ifdef PU_ENABLE
+    cy_en_prot_status_t status;
+
+    /* smpu */
+    status = smpu_protect(flash_spm_smpu_config, sizeof(flash_spm_smpu_config) / sizeof(flash_spm_smpu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = smpu_protect(sram_spm_smpu_config, sizeof(sram_spm_smpu_config) / sizeof(sram_spm_smpu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+
+    /* fixed region ppu */
+    status = ppu_fixed_rg_protect(fixed_rg_pc0_ppu_config, sizeof(fixed_rg_pc0_ppu_config) / sizeof(fixed_rg_pc0_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = ppu_fixed_rg_protect(fixed_rg_spm_ppu_config, sizeof(fixed_rg_spm_ppu_config) / sizeof(fixed_rg_spm_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = ppu_fixed_rg_protect(fixed_rg_any_ppu_config, sizeof(fixed_rg_any_ppu_config) / sizeof(fixed_rg_any_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    /* fixed slave ppu */
+    status = ppu_fixed_sl_protect(fixed_sl_pc0_ppu_config, sizeof(fixed_sl_pc0_ppu_config) / sizeof(fixed_sl_pc0_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = ppu_fixed_sl_protect(fixed_sl_spm_ppu_config, sizeof(fixed_sl_spm_ppu_config) / sizeof(fixed_sl_spm_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = ppu_fixed_sl_protect(fixed_sl_any_ppu_config, sizeof(fixed_sl_any_ppu_config) / sizeof(fixed_sl_any_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    /* programmable ppu */
+    status = ppu_prog_protect(prog_pc0_ppu_config, sizeof(prog_pc0_ppu_config) / sizeof(prog_pc0_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = ppu_prog_protect(prog_spm_ppu_config, sizeof(prog_spm_ppu_config) / sizeof(prog_spm_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    /* fixed group ppu */
+    status = ppu_fixed_gr_protect(fixed_gr_pc0_ppu_config, sizeof(fixed_gr_pc0_ppu_config) / sizeof(fixed_gr_pc0_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+    status = ppu_fixed_gr_protect(fixed_gr_spm_ppu_config, sizeof(fixed_gr_spm_ppu_config) / sizeof(fixed_gr_spm_ppu_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+
+    /* bus masters */
+    status = bus_masters_protect(bus_masters_config, sizeof(bus_masters_config) / sizeof(bus_masters_config[0]));
+    CY_ASSERT(status == CY_PROT_SUCCESS);  // TODO: Panic instead
+#endif // PU_ENABLE
 }
