f3a481a1cfcc5338ad2773aad48a48b51768b7c8
Oren Cohen <oren.cohen@arm.com>
2018-11-05 18:56:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-BASE_DIR=$(basename $(pwd))
+# Build secure images
+mbed compile -m FUTURE_SEQUANA_M0_PSA -t GCC_ARM -c --profile debug -N psoc63_m0_psa
+mbed test --compile -m FUTURE_SEQUANA_M0_PSA -t GCC_ARM -c --profile debug -n tests-spm-*
 
-mbed compile -m FUTURE_SEQUANA_M0_PSA -t GCC_ARM -c --profile debug
-cp BUILD/FUTURE_SEQUANA_M0_PSA/GCC_ARM/$BASE_DIR.hex targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8C63XX/TARGET_MCU_PSOC6_M4/device/psoc63_m0_psa_1.0.hex
-mbed test --compile -m FUTURE_SEQUANA_PSA -t GCC_ARM -n psa-services-secure_add-tests-secure_add-dummy --profile debug -c -DNO_GREENTEA
-cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/psa/services/secure_add/TESTS/secure_add/dummy/dummy.hex /media/$USER/KitProg2
+#Build Non-secure images
+mbed test --compile -m FUTURE_SEQUANA_PSA -t GCC_ARM -n tests-mbed_hal-spm,tests-psa-*,tests-spm-* --profile debug -c -DNO_GREENTEA
 
-mbed compile -m FUTURE_SEQUANA_M0_PSA -t GCC_ARM -c --profile debug --source . --source TESTS/spm/smoke/TARGET_PSA_TFM
-cp BUILD/FUTURE_SEQUANA_M0_PSA/GCC_ARM/$BASE_DIR.hex targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8C63XX/TARGET_MCU_PSOC6_M4/device/psoc63_m0_psa_1.0.hex
-mbed test --compile -m FUTURE_SEQUANA_PSA -t GCC_ARM -n tests-spm-smoke --profile debug -c -DNO_GREENTEA
+# This line builds all the tests of mbed-os
+# mbed test --compile -m FUTURE_SEQUANA_PSA -t GCC_ARM --profile debug -c -DNO_GREENTEA
+
+cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/TESTS/psa/secure_add/secure_add.hex /media/$USER/KitProg2
+cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/TESTS/psa/prot-internal-storage/prot-internal-storage.hex /media/$USER/KitProg2
+cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/TESTS/mbed_hal/spm/spm.hex /media/$USER/KitProg2
 cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/TESTS/spm/smoke/smoke.hex /media/$USER/KitProg2
-
-mbed compile -m FUTURE_SEQUANA_M0_PSA -t GCC_ARM -c --profile debug --source . --source TESTS/spm/client_tests/TARGET_PSA_TFM
-cp BUILD/FUTURE_SEQUANA_M0_PSA/GCC_ARM/$BASE_DIR.hex targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8C63XX/TARGET_MCU_PSOC6_M4/device/psoc63_m0_psa_1.0.hex
-mbed test --compile -m FUTURE_SEQUANA_PSA -t GCC_ARM -n tests-spm-client_tests --profile debug -c -DNO_GREENTEA
 cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/TESTS/spm/client_tests/client_tests.hex /media/$USER/KitProg2
-
-mbed compile -m FUTURE_SEQUANA_M0_PSA -t GCC_ARM -c --profile debug --source . --source TESTS/spm/server/TARGET_PSA_TFM
-cp BUILD/FUTURE_SEQUANA_M0_PSA/GCC_ARM/$BASE_DIR.hex targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8C63XX/TARGET_MCU_PSOC6_M4/device/psoc63_m0_psa_1.0.hex
-mbed test --compile -m FUTURE_SEQUANA_PSA -t GCC_ARM -n tests-spm-server --profile debug -c -DNO_GREENTEA
 cp BUILD/tests/FUTURE_SEQUANA_PSA/GCC_ARM/TESTS/spm/server/server.hex /media/$USER/KitProg2
