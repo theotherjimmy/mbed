09a94e4d19ec49951be8c220260a1f7133a1142a
Jimmy Brisson <theotherjimmy@gmail.com>
2019-02-28 10:38:29 -0600

    [REMOVE BEFORE PR] Useful development files

---
+++ run-bmtests.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+
+# Use this with `watchexec -w . -f cpp -- "fish run-bmtests.fish"` to
+# auto retest when you update or add a test
+
+mbed test --compile -t gcc_arm -m nucleo_f411re -n "*bare-metal*" --app-config bm-app.json
+mbed test --run -t gcc_arm -m nucleo_f411re -n "*bare-metal*"
